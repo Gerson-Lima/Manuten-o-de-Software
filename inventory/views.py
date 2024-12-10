85f23fdf32362cc8eb956ecde0ba4c606c5b114a
Gerson-Lima <gersondouglas2011@gmail.com>
2024-12-10 17:51:00 -0300

    add: try exception

--- inventory/views.py
+++ inventory/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import status, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
 from .models import Category, Item
 from .serializers import CategorySerializer, ItemSerializer
+
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
@@ -11,3 +15,28 @@ class ItemViewSet(viewsets.ModelViewSet):
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
 
+    @action(detail=False, methods=['get'])
+    def filter_by_category(self, request):
+
+        category_name = request.query_params.get('category', None)
+        
+        if not category_name:
+            return Response(
+                {"error": "O parâmetro 'category' é obrigatório."}, 
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        
+        try:
+            category = Category.objects.get(name=category_name)
+            items = category.items.all()
+            serializer = self.get_serializer(items, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Category.DoesNotExist:
+            return Response(
+                {"error": f"Categoria '{category_name}' não encontrada."}, 
+                status=status.HTTP_404_NOT_FOUND
+            )
+        except Exception as e:
+            return Response(
+                {"error": "Ocorreu um erro inesperado.", "details": str(e)}, 
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR)
